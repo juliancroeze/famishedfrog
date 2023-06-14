01ae90b5417741420b6327ab7418ba81231aa3c0
Nielsnlamf <nielsnlamf@gmail.com>
2023-06-14 14:36:45 +0200

    .

--- Famished Frog/Assets/ButtonScript.cs
+++ Famished Frog/Assets/ButtonScript.cs
@@ -1,18 +1,23 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class ButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        onClick();
     }
 
     // Update is called once per frame
     void onClick()
     {
-        Destroy(this.gameObject);
+        SceneManager.LoadScene(sceneName: "SampleScene");
+        Thread.Sleep(10000);
+        SceneManager.LoadScene(sceneName: "Scenes/SampleScene");
+
     }
 }
